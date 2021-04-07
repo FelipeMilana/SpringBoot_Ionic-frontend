3c9ad079213fe43385d7e8ed9a5cdaf1ca3577b1
Felipe Milana <felipe.milana97@gmail.com>
2021-04-07 15:59:40 -0300

    Handling errors with Alerts

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular/components/alert/alert-controller";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 
@@ -7,7 +8,8 @@ import { StorageService } from "../services/storage.service";
 export class ErrorInterceptor implements HttpInterceptor {
 
     constructor(
-        public storage: StorageService){
+        public storage: StorageService,
+        public alertCtrl: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -27,17 +29,52 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
-                break;     
+                break;  
+                
+                default:
+                this.handleDefaultError(errorObj);   
             }
             
             return Observable.throw(errorObj);
         }) as any;
     }   
 
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha inválidos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(errorObj: any) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
