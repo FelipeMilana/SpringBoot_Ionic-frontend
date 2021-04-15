50bd3b7cc18c208a8a5aa52f81619cee824278f4
Felipe Milana <felipe.milana97@gmail.com>
2021-04-15 19:36:59 -0300

    Loading Controller

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProductDTO } from '../../models/product.dto';
 import { ProductService } from '../../services/domain/product.service';
@@ -15,20 +16,32 @@ export class ProductsPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public productService: ProductService) {
+    public productService: ProductService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoryId = this.navParams.get('id');
-
+    let loader = this.presentLoading();
     this.productService.findByCategories(categoryId)
       .subscribe(response => {
         this.items = response['content'];
+        loader.dismiss();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
   }
 
   showDetails(itemId: string) {
     this.navCtrl.push('ProductDetailPage', {id: itemId});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde...",
+    });
+    loader.present();
+    return loader;
   }
 }
