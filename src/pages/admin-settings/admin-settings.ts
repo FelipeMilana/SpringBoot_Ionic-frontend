9654204ac02386726211ec1ed9048bb20be1d86a
Felipe Milana <felipe.milana97@gmail.com>
2021-04-16 19:57:08 -0300

    Implementing admin settings page

---
+++ src/pages/admin-settings/admin-settings.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoryDTO } from '../../models/category.dto';
+import { CategoryService } from '../../services/domain/category.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-admin-settings',
+  templateUrl: 'admin-settings.html',
+})
+export class AdminSettingsPage {
+
+  items: CategoryDTO[];
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoryService: CategoryService) {
+  }
+
+  ionViewDidLoad() {
+    this.categoryService.findAll()
+      .subscribe(response => {
+        this.items = response;
+      },
+      (error) => {}
+      );
+  }
+
+}
