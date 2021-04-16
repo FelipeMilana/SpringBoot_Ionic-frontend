091610503050529eaa85c308ee1d1563d6fb14be
Felipe Milana <felipe.milana97@gmail.com>
2021-04-16 18:25:18 -0300

    Implementing settings button in CategoriesPage to admin profiles

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoryDTO } from '../../models/category.dto';
 import { CategoryService } from '../../services/domain/category.service';
+import { ClientService } from '../../services/domain/client.service';
+import { StorageService } from '../../services/storage.service';
 
 
 @IonicPage()
@@ -12,11 +14,15 @@ import { CategoryService } from '../../services/domain/category.service';
 export class CategoriesPage {
 
   items: CategoryDTO[];
+  hasAdmin: boolean = false;
+  profiles: string[];
    
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
-    public categoryService: CategoryService) {
+    public categoryService: CategoryService,
+    public storage: StorageService,
+    public clientService: ClientService) {
   }
 
   ionViewDidLoad() {
@@ -26,9 +32,31 @@ export class CategoriesPage {
       },
       (error) => {}
       );
+
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email) {
+      this.clientService.findByEmail(localUser.email)
+       .subscribe(response => {
+        this.profiles = response['profiles'];
+            
+        let position = this.profiles.indexOf('ADMIN');
+  
+        if(position != -1) {
+          this.hasAdmin = true;
+        }
+      },
+      error => {
+        if(error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+    }
+    else {
+     this.navCtrl.setRoot('HomePage');
+    }
   }
 
   showProducts(categoryId: string) {
     this.navCtrl.push('ProductsPage', {id: categoryId});  
-  }
+  } 
 }
